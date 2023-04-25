2028bdcf553216049d54c34a4db43512e2de758e
franciscoferretti <fjferretti.d@gmail.com>
2023-04-24 22:31:34 -0300

    iniciando ejercicios

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,35 +4,44 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string
 }
-
+let resultadoString = devolverString("Hola como estas")
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
-   // Tu código:
+   // Tu código: 
+   return x + y;
 }
-
+let resultado = suma(5, 5);
+console.log(resultado);
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y ;
 }
+let resultadoResta = resta(25, 5);
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y ;
 }
+let resultadoDivide = divide(123, 3);
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x * y;
 }
-
+let resultadoMultiplica = multiplica(124, 5);
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return x % y ;
 }
-
+let resultadoResto = obtenerResto(251, 32);
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
    devolverString,
--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,37 +5,76 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   if(x === y){
+      return true;
+   }else{
+      return false
+   }
 }
+let resutadoIguales = sonIguales(7,"7");
+console.log(resutadoIguales)
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   if(str1.length === str2.length){
+      return true;
+   }else{
+      return false;
+   }
 }
-
+let resultadoLongitud = tienenMismaLongitud("holaaaaaa", "holaaaaaaaaaaa");
+console.log( resultadoLongitud);
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   if(num < 90){
+      return true;
+   }else{
+      return false;
+   }
 }
+let resmenorqnoventa = menosQueNoventa(125);
+console.log(resmenorqnoventa);
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   if(num > 50){
+      return true;
+   }else{
+      return false;
+   }
 }
-
+let resmayorcincuenta =  mayorQueCincuenta(52);
+console.log(resmayorcincuenta);
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   if( num % 2 === 0){
+      return true;
+   }else{
+      return false;
+   }
 }
-
+let resespar = esPar(13);
+console.log(resespar);
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   if( num % 2 === 1){
+      return true;
+   }else{
+      return false;
+   }
 }
+let resesimpar = esPar(10);
+console.log(resesimpar);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
