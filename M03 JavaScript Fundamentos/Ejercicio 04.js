d8c62716a1e6bdb34bcd74bf4d27cc502d2e871b
franciscoferretti <fjferretti.d@gmail.com>
2023-04-25 00:56:00 -0300

    m03 -eje04

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,34 +6,54 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   return num ** 2;
 }
-
+let resultadoCuadrado = elevarAlCuadrado(5);
+console.log(resultadoCuadrado);
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   return num ** 3;
 }
+let resultadoCubo = elevarAlCubo(5);
+console.log(resultadoCubo);
+
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   return Math.pow(num,exponent);
 }
-
+let resultadoElevar = elevar(2,8);
+console.log(resultadoElevar);
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   return Math.round(num);
 }
+let resultadoRedondeo = redondearNumero(3.4);
+console.log(resultadoRedondeo);
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   return Math.ceil(num);
 }
-
+console.log(redondearHaciaArriba(3.1));
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   let aleatorio = Math.random();
+   return aleatorio;
 }
+console.log(numeroRandom());
+// -----------
+// function numeroRandom2(min,max){
+// return Math.floor(Math.random() * (max - min + 1) + min);
+// }
+// console.log( numeroRandom2(1,15)); 
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
